ffedea0fe510c2a03c705d5417586b7f3451b99c
TrickyNoodle <sagarshinde2k20@gmail.com>
2025-09-08 18:37:40 +0530

    Fixed an issue with CORS Header,now can accept request from any source

--- backend/app.py
+++ backend/app.py
@@ -6,21 +6,7 @@ from flask_cors import CORS,cross_origin
 
 app = Flask(__name__)
 
-
-#CORS
-CORS(
-    app,
-    supports_credentials=True, # Add This
-    origins=['null'], # You'll need this, you cannot use * (wildcard domain) when using supports_credentials=True
-)
-@app.before_request
-def before_request():
-    headers = {'Access-Control-Allow-Origin': '*',
-               'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-               'Access-Control-Allow-Headers': 'Content-Type',
-               'Test-Header': 'Test-Value'}
-    if request.method.lower() == 'options':
-        return jsonify(headers), 200
+CORS(app,send_wildcard=True)
 
 model = tf.keras.models.load_model("models/densenet121/densenet121_dataset1-5.keras")
 
